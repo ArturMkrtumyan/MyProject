b7a7f62aa9ac5fe7a5fe59be76eb320767653119
Artur Mkrtumyan <artur_mkrtumyan@epam.com>
2023-10-16 19:37:03 +0400

    add test

--- src/test/java/com/example/MyProject/MyProjectApplicationTests.java
+++ src/test/java/com/example/MyProject/MyProjectApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.MyProject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MyProjectApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void testGetHello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Ci Jenkins"));
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
